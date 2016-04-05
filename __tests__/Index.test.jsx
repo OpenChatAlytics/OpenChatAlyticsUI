df32036c5063c93be08b338623c4d25853a52fab
Paul Sastrasinh <psastras@gmail.com>
2016-04-05 01:59:22 -0400

    Fix Tests
    Mock out the store

--- __tests__/Index.test.jsx
+++ __tests__/Index.test.jsx
@@ -1,16 +1,23 @@
-jest.dontMock('../client/components/Index/Index.jsx');
+jest.disableAutomock();
+jest.mock('../client/stores/MainStore.js');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-var Index = require('../client/components/Index/Index.jsx').default;
-
 describe('IndexComponent', () => {
+  
+  beforeEach(() => {
+    const MockMainStore = require('../client/stores/MainStore.js');
+    MockMainStore.getState.mockImplementation(() => {
+      return { locations: [] }
+    });
+  });
 
   it('should render', () => {
+    var Index = require('../client/components/Index/Index.jsx').default;
     var componentInstance = TestUtils.renderIntoDocument(<Index />);
-    var indexItems = TestUtils.scryRenderedDOMComponentsWithTag(componentInstance, 'h1');
+    var indexItems = TestUtils.scryRenderedDOMComponentsWithTag(componentInstance, 'ul');
 
     expect(indexItems).not.toBeNull();
   });
