8804131375fff55abecf3c885f1600e8239b4feb
Paul Sastrasinh <paul.sastrasinh@knewton.com>
2016-11-21 10:17:08 -0500

    fix enzyme testing

---
+++ webpack.config.test.js
@@ -0,0 +1,25 @@
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  output: {
+    // sourcemap support for IntelliJ/Webstorm
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+  },
+  resolve: {
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  module: {
+    devtool: 'cheap-module-source-map',
+    loaders: [
+      { test: /\.tsx?$/, loaders: ['ts-loader'] },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+      { test: /\.(jpe?g|png|gif|svg|eot|woff|svg|ttf)/, loader: 'file' },
+    ],
+    preLoaders: [
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+};
