1cb9305cbefd44363f0ffad21d1b749ad6b47841
Paul Sastrasinh <psastras@gmail.com>
2016-11-22 19:34:21 -0500

    add test source map support

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,34 +1,35 @@
-const nodeExternals = require('webpack-node-externals');
-const path = require('path');
-const glob = require('glob');
-const _ = require('lodash');
+const nodeExternals = require("webpack-node-externals");
+const path = require("path");
+const glob = require("glob");
+const _ = require("lodash");
 
 module.exports = {
-  target: 'node', // in order to ignore built-in modules like path, fs, etc.
-  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  devtool: "source-map",
   entry: _.keyBy(glob.sync("./test/**/*.ts*"), (key) => key),
-  output: {
-    path: __dirname + "/.build",
-    filename: "[name].js",
-    // sourcemap support for IntelliJ/Webstorm
-    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
-  },
-  resolve: {
-    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-    alias: {
-      src: path.join(__dirname, '/src')
-    }
-  },
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
-    devtool: 'cheap-module-source-map',
+    devtool: "cheap-module-source-map",
     loaders: [
-      { test: /\.tsx?$/, loaders: ['ts-loader'] },
-      { test: /\.scss$/, loader: 'style!css!sass' },
-      { test: /\.(jpe?g|png|gif|svg|eot|woff|svg|ttf)/, loader: 'file' },
+      { test: /\.tsx?$/, loaders: ["ts-loader"] },
+      { test: /\.scss$/, loader: "style!css!sass" },
+      { test: /\.(jpe?g|png|gif|svg|eot|woff|svg|ttf)/, loader: "file" },
     ],
     preLoaders: [
       { test: /\.js$/, loader: "source-map-loader" }
-    ]
+    ],
   },
+  output: {
+    // sourcemap support for IntelliJ/Webstorm
+    devtoolFallbackModuleFilenameTemplate: "[absolute-resource-path]?[hash]",
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
+    filename: "[name].js",
+    path: __dirname + "/.build",
+  },
+  resolve: {
+    alias: {
+      src: path.join(__dirname, "/src")
+    },
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  target: "node", // in order to ignore built-in modules like path, fs, etc.
 };
