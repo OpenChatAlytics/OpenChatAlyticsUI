66292f89dff97416764ea00ae00c2c658e6690d9
Paul Sastrasinh <psastras@gmail.com>
2016-11-27 09:27:20 -0500

    set up router

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,30 @@ import * as ReactNotificationSystem from 'react-notification-system';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
-import { Router, Route, browserHistory  } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import Home from './pages/home';
+import Navbar from './pages/components/navbar';
+import Rooms from './pages/components/rooms';
+import Users from './pages/components/users';
 import * as Actions from 'src/flux/actions';
 import store from './flux';
 import './index.scss';
 
+const navbar = (component: any) => React.createClass({
+  render() {
+    return (
+      <div>
+        <Navbar />
+        {component}
+      </div>
+    );
+  },
+});
+
 class Index extends React.Component<any, any> {
 
   public readonly refs: {
-      notification_system: Element;
+    notification_system: Element;
   };
 
   public componentDidMount() {
@@ -26,7 +40,9 @@ class Index extends React.Component<any, any> {
       <div>
         <ReactNotificationSystem ref='notification_system' />
         <Router history={browserHistory}>
-          <Route path='*' component={Home} />
+          <Route path='/rooms' component={navbar(<Rooms />)} />
+          <Route path='/users' component={navbar(<Users />)} />
+          <Route path='*' component={navbar(<Home />)} />
         </Router>
       </div>
     );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Navbar from './components/navbar';
 import Banner from './components/banner';
 import Rooms from './components/rooms';
 import Users from './components/users';
@@ -9,10 +8,7 @@ export default class Home extends React.Component<any, {}> {
   public render() {
     return (
       <div>
-        <Navbar />
         <Banner />
-        <Users />
-        <Rooms />
       </div>
     );
   }
