0b86eac894e7449869a12cea49799ab1090a5b84
Paul Sastrasinh <psastras@gmail.com>
2016-11-27 03:06:54 -0500

    move notification component up one level

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,49 @@
 // tslint:disable-next-line:no-reference
 /// <reference path="../typings/index.d.ts"/>
 
+import * as ReactNotificationSystem from 'react-notification-system';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { Router, Route, browserHistory  } from 'react-router';
 import Home from './pages/home';
+import * as Actions from 'src/flux/actions';
 import store from './flux';
 import './index.scss';
 
+class Index extends React.Component<any, any> {
+
+  public readonly refs: {
+      notification_system: Element;
+  };
+
+  public componentDidMount() {
+    this.props.notifyInit(this.refs.notification_system);
+  }
+
+  public render() {
+    return (
+      <div>
+        <ReactNotificationSystem ref='notification_system' />
+        <Router history={browserHistory}>
+          <Route path='*' component={Home} />
+        </Router>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  notifyInit: (container) => dispatch(Actions.notifyInit(container)),
+});
+
+// tslint:disable-next-line:variable-name
+const IndexProvider = connect(mapStateToProps, mapDispatchToProps)(Index);
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path='*' component={Home} />
-    </Router>
+    <IndexProvider />
   </Provider>,
   document.getElementById('container'),
 );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,27 +1,14 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
 import Navbar from './components/navbar';
 import Banner from './components/banner';
 import Rooms from './components/rooms';
 import Users from './components/users';
-import * as Actions from 'src/flux/actions';
-import * as ReactNotificationSystem from 'react-notification-system';
 import './home.scss';
 
-export class Home extends React.Component<any, {}> {
-
-  public readonly refs: {
-      notification_system: Element;
-  };
-
-  public componentDidMount() {
-    this.props.notifyInit(this.refs.notification_system);
-  }
-
+export default class Home extends React.Component<any, {}> {
   public render() {
     return (
       <div>
-        <ReactNotificationSystem ref='notification_system' />
         <Navbar />
         <Banner />
         <Users />
@@ -30,10 +17,3 @@ export class Home extends React.Component<any, {}> {
     );
   }
 }
-
-const mapStateToProps = (state, props) => ({});
-const mapDispatchToProps = (dispatch) => ({
-  notifyInit: (container) => dispatch(Actions.notifyInit(container)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
