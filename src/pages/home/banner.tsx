bd609682de1d0a9e20f1e708d4408abacbaee260
Paul Sastrasinh <psastras@gmail.com>
2016-11-27 22:25:23 -0500

    parallax banner

--- src/pages/home/banner.tsx
+++ src/pages/home/banner.tsx
@@ -3,23 +3,28 @@ import { Grid, Row, Button, Jumbotron } from 'react-bootstrap';
 import * as Actions from '../../../src/flux/actions';
 import { connect } from 'react-redux';
 import * as ReactNotificationSystem from 'react-notification-system';
+// tslint:disable-next-line:no-var-requires
+const Parallax = require('react-parallax').Parallax;
+import './banner.scss';
 
 class Banner extends React.Component<any, {}> {
   public render() {
     return (
-      <Jumbotron id='banner'>
-        <Grid>
-          <Row>
-            <h1>Open Chatalytics</h1>
-            <p>Open source real time chat analytics.</p>
-            <p>
-              <Button bsStyle='primary'>
-                GitHub <i className='fa fa-github'></i>
-              </Button>
-            </p>
-          </Row>
-        </Grid>
-      </Jumbotron>
+      <Parallax bgImage='src/assets/images/banner.jpg' strength={150}>
+        <Jumbotron id='banner'>
+          <Grid>
+            <Row>
+              <h1>Open Chatalytics</h1>
+              <p>Open source real time chat analytics.</p>
+              <p>
+                <Button bsStyle='primary'>
+                  GitHub <i className='fa fa-github'></i>
+                </Button>
+              </p>
+            </Row>
+          </Grid>
+        </Jumbotron>
+      </Parallax>
     );
   }
 }
@@ -30,7 +35,7 @@ const mapStateToProps = (state) => {
   };
 };
 const mapDispatchToProps = (dispatch) => ({
-    noop: (field, step) => dispatch(Actions.noop()),
+  noop: (field, step) => dispatch(Actions.noop()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Banner);
